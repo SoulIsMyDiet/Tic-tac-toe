0bc87bdeb133ea2cd610915db8d24049fc5ad5ad
Anzelm <marekd.90@gmail.com>
2023-11-04 19:35:52 +0100

    refactor to make gameturns instead of gameStructure state

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,26 @@ import BoardGame from "./components/BoardGame";
 import {useState} from 'react';
 function App() {
     const [activePlayer, setActivePlayer] = useState('X');
+    const [gameTurns, setGameTurns] = useState([]);
 
-    function changeActivePlayer() {
+    function handleSelectSquare(rowIndex, colIndex) {
         setActivePlayer(prevActivePlayer => {
             if(prevActivePlayer === 'X'){
                 return 'O';
             } else {
                 return 'X';
             }
-        })
+        });
+        setGameTurns(prevTurn => {
+                let player = 'X';
+                if (prevTurn.length > 0 && prevTurn[0].player === 'X') {
+                    player = 'O';
+                }
+                const gameTurns = [{square: {row: rowIndex, col: colIndex}, player: player},
+                    ...prevTurn];
+
+                return gameTurns;
+            });
     }
     return (
         <main>
@@ -20,7 +31,7 @@ function App() {
                     <Player isActive={activePlayer === 'X'} name='Player1' symbol='X'/>
                     <Player isActive={activePlayer === 'O'} name='Player2' symbol='O'/>
                 </ol>
-                <BoardGame swapPlayers={changeActivePlayer} symbol={activePlayer}/>
+                <BoardGame selectSquare={handleSelectSquare} gameTurns={gameTurns}/>
             </div>
             Log
         </main>
--- src/components/BoardGame.jsx
+++ src/components/BoardGame.jsx
@@ -6,16 +6,22 @@ const initialBoardgameStructure = [
     [null, null, null],
 ];
 
-export default function BoardGame({swapPlayers, symbol}) {
-    const [boardgameStructure, setBoardgameStructure] = useState(initialBoardgameStructure);
-function handleSelectSquare(rowIndex, colIndex) {
-    setBoardgameStructure((prevBoardGameStructure) => {
-        const board = [...prevBoardGameStructure.map(innerArray => [...innerArray])];
-        board[rowIndex][colIndex] = symbol;
-        return board;
-    });
-    swapPlayers();
-}
+export default function BoardGame({selectSquare, gameTurns}) {
+    let boardgameStructure = initialBoardgameStructure;
+    for (const turn of gameTurns){
+        let {square, player} = turn;
+        let {row, col } = square;
+        boardgameStructure[row][col] = player;
+    }
+    // const [boardgameStructure, setBoardgameStructure] = useState(initialBoardgameStructure);
+// function handleSelectSquare(rowIndex, colIndex) {
+//     setBoardgameStructure((prevBoardGameStructure) => {
+//         const board = [...prevBoardGameStructure.map(innerArray => [...innerArray])];
+//         board[rowIndex][colIndex] = symbol;
+//         return board;
+//     });
+//     swapPlayers();
+// }
     return (
         <ol id="game-board">
             {boardgameStructure.map((row, rowIndex) => (
@@ -23,7 +29,7 @@ function handleSelectSquare(rowIndex, colIndex) {
                     <ol>
                         {row.map((symbol, columnIndex) => (
                             <li key={columnIndex}>
-                                <button onClick={() => handleSelectSquare(rowIndex, columnIndex)}>{symbol}</button>
+                                <button onClick={() => selectSquare(rowIndex, columnIndex)}>{symbol}</button>
                             </li>
                         ))}
                     </ol>
